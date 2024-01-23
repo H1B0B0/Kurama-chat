a368bad4b635ea7c86fed75a0b2efb754be656ce
etienne <etienne.mentrel@scafe.io>
2024-01-23 15:00:40 +0100

    Remove unnecessary files and update MongoDB initialization

--- back/init-mongo.js
+++ back/init-mongo.js
@@ -1,56 +1,56 @@
-require("dotenv").config();
+require("dotenv").config()
 
 db.createUser({
-  user: process.env.MONGODB_USER,
-  pwd: process.env.MONGODB_USER_PASSWORD,
-  roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }],
-});
+	user: process.env.MONGODB_USER,
+	pwd: process.env.MONGODB_USER_PASSWORD,
+	roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }],
+})
 
 db.createCollection("messages", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["conversationId", "messages"],
-      properties: {
-        conversationId: {
-          bsonType: "objectId",
-        },
-        messages: {
-          bsonType: "array",
-          items: {
-            bsonType: "object",
-            required: ["timestamp", "content"],
-            properties: {
-              timestamp: {
-                bsonType: "date",
-              },
-              content: {
-                bsonType: "string",
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-});
+	validator: {
+		$jsonSchema: {
+			bsonType: "object",
+			required: ["conversationId", "messages"],
+			properties: {
+				conversationId: {
+					bsonType: "objectId",
+				},
+				messages: {
+					bsonType: "array",
+					items: {
+						bsonType: "object",
+						required: ["timestamp", "content"],
+						properties: {
+							timestamp: {
+								bsonType: "date",
+							},
+							content: {
+								bsonType: "string",
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+})
 
 db.createCollection("users", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      required: ["id", "username", "password"],
-      properties: {
-        id: {
-          bsonType: "objectId",
-        },
-        username: {
-          bsonType: "string",
-        },
-        password: {
-          bsonType: "string",
-        },
-      },
-    },
-  },
-});
+	validator: {
+		$jsonSchema: {
+			bsonType: "object",
+			required: ["id", "username", "password"],
+			properties: {
+				id: {
+					bsonType: "objectId",
+				},
+				username: {
+					bsonType: "string",
+				},
+				password: {
+					bsonType: "string",
+				},
+			},
+		},
+	},
+})

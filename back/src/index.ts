e1f9bb2956d8ae68a17240c81daecfc4a4357da4
etienne <etienne.mentrel@scafe.io>
2024-01-23 15:16:46 +0100

    Update Docker configurations and fix port number

--- back/src/index.ts
+++ back/src/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");

e23474c1a13fc5d0b4dc037fc0c8349f6000fea8
etienne <etienne.mentrel@scafe.io>
2024-01-29 13:34:17 +0100

    Update S3 backend configuration
    Update region and bucket values in the S3 backend configuration in provider.tf file.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,11 +3,6 @@ variable "region" {
   default = "eu-west-3"
 }
 
-variable "bucket" {
-  type    = string
-  default = "stg16statebucket"
-}
-
 variable "ami" {
   type    = string
   default = "ami-0c03e02984f6a0b41"

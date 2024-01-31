01d5338ba77943fed78eae771ccda7996d144811
etienne <etienne.mentrel@scafe.io>
2024-01-31 13:44:12 +0100

    Add Traefik certificates and update security group rules

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -3,11 +3,11 @@ resource "aws_security_group" "allow_ssh_http_sg" {
   description = "Allows Port 22 and 80"
 
   ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
@@ -24,6 +24,15 @@ resource "aws_security_group" "allow_ssh_http_sg" {
     description      = "Allow HTTP"
     from_port        = 80
     to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "Allow HTTP"
+    from_port        = 8080
+    to_port          = 8080
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]

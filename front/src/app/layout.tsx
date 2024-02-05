4f982d5a34c6d8256e9435cff1fa605934b9c8aa
etienne <etienne.mentrel@scafe.io>
2024-02-05 14:38:45 +0100

    Update docker-compose.yml and layout.tsx

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,19 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Real time Chat',
-  description: 'Real time chat application',
-}
+  title: "Real time Chat",
+  description: "Real time chat application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className='bg-[url("/images/bg.jpg")] min-h-screen bg-cover bg-no-repeat bg-[#050505AB] bg-blend-overlay'>{children}</body>
+      <body className='bg-[url("/images/bg.jpg")] min-h-screen bg-cover bg-no-repeat bg-[#050505AB] bg-blend-overlay'>
+        {children}
+      </body>
     </html>
-  )
+  );
 }

6a6ca5321d1a09539b4d012c63bdb4eb237eb5dc
H1B0B0 <hibosmurf@gmail.com>
2024-02-09 16:30:30 +0100

    Add connection wait in init-mongo.sh

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
+until mongo --eval "print(\"waited for connection\")"
+do
+        sleep 1
+done
+
 mongo <<EOF
 use $MONGO_INITDB_DATABASE
 db.createUser({
-  user:  "$MONGODB_USER",
-  pwd: "$MONGODB_USER_PASSWORD",
-  roles: [{
-    role: "readWrite",
-    db: "$MONGO_INITDB_DATABASE"
-  }]
+    user: "$MONGODB_USER",
+    pwd: "$MONGODB_USER_PASSWORD",
+    roles: [{
+        role: "readWrite",
+        db: "$MONGO_INITDB_DATABASE"
+    }]
 })
 EOF
